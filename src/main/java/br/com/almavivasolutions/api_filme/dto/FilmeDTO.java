905a4aaaeadb83e0bfdebb61c9af515bce146b93
Gustavo Goulart <goulartgusta9@gmail.com>
2025-01-09 14:34:08 -0300

    integrando gitlab

---
+++ src/main/java/br/com/almavivasolutions/api_filme/dto/FilmeDTO.java
@@ -0,0 +1,24 @@
+package br.com.almavivasolutions.api_filme.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+import br.com.almavivasolutions.api_filme.models.Classificacao;
+import br.com.almavivasolutions.api_filme.models.Diretor;
+import br.com.almavivasolutions.api_filme.models.Distribuidora;
+import br.com.almavivasolutions.api_filme.models.Patrocinador;
+import br.com.almavivasolutions.api_filme.models.Produtora;
+import br.com.almavivasolutions.api_filme.models.Roteiro;
+
+public record FilmeDTO(
+		String nome, 
+		Integer duracaoMinuto, 
+		LocalDate dataLancamento, 
+		Classificacao classificacao,
+		Roteiro roteiro, 
+		Diretor diretor, 
+		Distribuidora distribuidora, 
+		List<Produtora> produtora,
+		List<Patrocinador> patrocinador) {
+
+}
